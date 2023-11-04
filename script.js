c85dc396185400404678ba033cab9b16402edff4
Amer Adel <80412883+Amer-Adel@users.noreply.github.com>
2023-11-04 19:32:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let navMenu = document.querySelector("nav");
-
-let navIcon = document.querySelector("header i");
-navIcon.onclick = () => {
-  navIcon.classList.toggle("fa-x");
-  navIcon.classList.toggle("fa-bars");
-  navMenu.style.display = navIcon.classList.contains("fa-x") ? "flex" : "none";
-}
-
 if (window.screen.width < 768) {
+  let navMenu = document.querySelector("nav");
+
+  let navIcon = document.querySelector("header i");
+  navIcon.onclick = () => {
+    navIcon.classList.toggle("fa-x");
+    navIcon.classList.toggle("fa-bars");
+    navMenu.style.display = navIcon.classList.contains("fa-x") ? "flex" : "none";
+  }
+
   let navLinks = document.querySelectorAll("nav a");
   for (let i = 0; i < navLinks.length; i++)
     navLinks[i].onclick = () => {
-      navIcon.classList.toggle("fa-x");
-      navIcon.classList.toggle("fa-bars");
+      navIcon.classList.remove("fa-x");
+      navIcon.classList.add("fa-bars");
       navMenu.style.display = "none";
     }
 }
